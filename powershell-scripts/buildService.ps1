80a09f883f53824d1fe4b429ad3950e6c60f6dc4
VintageMachine <danielpeter.mnz@gmail.com>
2024-03-20 15:20:13 +0100

    Modified some files and jar name file for videogamestore

--- powershell-scripts/buildService.ps1
+++ powershell-scripts/buildService.ps1
@@ -2,33 +2,18 @@ function call {
     Write-Host "VideogameService initialized"
 }
 
-function installDependencies() {
-    [Environment]::SetEnvironmentVariable("JAVA_HOME", "/bin/java", [EnvironmentVariableTarget]::Machine)
-}
-
 function createJarFile {
     param(
         [string]$Path
     )
     try {
-        # Install OpenJDK 21 using apt
         sudo apt update
-        sudo apt install openjdk-11-jdk -y
-
-        # Set JAVA_HOME environment variable
+        sudo apt install openjdk-21-jdk -y
         $env:JAVA_HOME = "/usr/lib/jvm/java-1.21.0-openjdk-amd64"
-
-        # Update PATH to include JDK bin directory
         $env:PATH = "$env:JAVA_HOME/bin;$env:PATH"
-
-        # Confirm Java version
         java -version
-
-        # Navigate to the specified path
         Set-Location $Path
         Write-Host "Current Directory: $(Get-Location)"
-
-        # Run Maven commands
         mvn -v
         mvn clean install
     }
@@ -36,11 +21,6 @@ function createJarFile {
         Write-Host "Error occurred: $_"
     }
 }
-
-# Call the function with the desired path
-createJarFile -Path "store-videogame-products-example"
-
-
 
 function buildAndPushOnDocker {
     param(
