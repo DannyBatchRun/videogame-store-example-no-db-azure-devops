c3795080e2e801f35c12c1412e36bbc953e62d05
VintageMachine <danielpeter.mnz@gmail.com>
2024-03-21 10:43:19 +0100

    Modified example.ps1

--- powershell-scripts/example.ps1
+++ powershell-scripts/example.ps1
@@ -7,7 +7,8 @@ function callVar {
     [string]$UsernameDockerHub = "dannybatchrun"
     [string]$ImageName = "usersubscription"
     [string]$repositoryName = "$UsernameDockerHub/$ImageName"
-    [string]$lowercaseRepositoryName = $repositoryName.ToLower()
+    [string]$ImageComplete = "${repositoryName}:$ImageTag"
+    [string]$lowercaseRepositoryName = $ImageComplete.ToLower()
     Write-Output $lowercaserepositoryName
     Write-Output "Hey there!"
 }
