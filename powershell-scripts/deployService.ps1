ce84ffaa625e64a41d0b58d5083ca7b79a26784a
VintageMachine <danielpeter.mnz@gmail.com>
2024-03-22 10:03:11 +0100

    Test declaration

--- powershell-scripts/deployService.ps1
+++ powershell-scripts/deployService.ps1
@@ -22,13 +22,13 @@ function pullDockerImage {
         [string]$imageName,
         [string]$imageVersion
     )
-    Write-Host $imageName
     if ($deployAll -eq $true) {
         docker pull --% index.docker.io/dannybatchrun/usersubscription:$imageVersion
         docker pull --% index.docker.io/dannybatchrun/videogameproducts:$imageVersion
         docker pull --% index.docker.io/dannybatchrun/videogamestore:$imageVersion
     } elseif ($deployAll -eq $false) {
-        docker pull --% index.docker.io/dannybatchrun/$imageName:$imageVersion
+        [string]$repositoryName = "index.docker.io/dannybatchrun/$(imageName):$(imageVersion)".ToLower()
+        docker pull --% $repositoryName
     }
 }
 
