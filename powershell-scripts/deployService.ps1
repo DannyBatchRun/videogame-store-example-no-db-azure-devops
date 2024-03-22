906e974e6d75b97a23d3db93f45a1be6426361ac
VintageMachine <danielpeter.mnz@gmail.com>
2024-03-22 15:10:33 +0100

    Debugging deployService.ps1

--- powershell-scripts/deployService.ps1
+++ powershell-scripts/deployService.ps1
@@ -48,6 +48,7 @@ function upgradeHelmDeployment {
     $chartContent | Set-Content Chart.yaml
     helm package .
     kubectl scale --replicas=0 "deployment/${imageName}" -n "${imageName}"
+    Write-Host "**** Helm Upgrade Command: helm upgrade "${imageName}" . --set "image.repository=index.docker.io/dannybatchrun/${imageName},image.tag=${imageTag},image.pullPolicy=Always,service.port=${servicePort},livenessProbe.httpGet.path=/health,livenessProbe.httpGet.port=${servicePort},service.type=NodePort" -n "${imageName}" ****"
     helm upgrade "${imageName}" . --set "image.repository=index.docker.io/dannybatchrun/${imageName},image.tag=${imageTag},image.pullPolicy=Always,service.port=${servicePort},livenessProbe.httpGet.path=/health,livenessProbe.httpGet.port=${servicePort},service.type=NodePort" -n "${imageName}"
     kubectl scale --replicas=1 "deployment/${imageName}" -n "${imageName}"
 }
