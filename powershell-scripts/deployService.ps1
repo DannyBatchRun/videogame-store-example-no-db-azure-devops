87be659968a293dde435f52ffe171593eeed55cb
VintageMachine <danielpeter.mnz@gmail.com>
2024-03-26 09:42:00 +0100

    Modified deployService.ps1

--- powershell-scripts/deployService.ps1
+++ powershell-scripts/deployService.ps1
@@ -44,26 +44,15 @@ function upgradeHelmDeployment {
     Write-Host "**** Chart Version of Helm: $chartVersion ****"
     Set-Location "helm-integration/${imageName}"
     $chartContent = Get-Content Chart.yaml
-    $chartContent = ${chartContent} -replace '^version:.*', "version: ${chartVersion}"
+    $chartContent = $chartContent -replace '^version:.*', "version: ${chartVersion}"
     $chartContent | Set-Content Chart.yaml
     helm package .
     kubectl scale --replicas=0 "deployment/${imageName}" -n "${imageName}"
-    $helmUpgradeCommand = "helm"
-    $helmUpgradeArgs = @(
-        "upgrade",
-        $imageName,
-        ".",
-        "--set", "image.repository=index.docker.io/dannybatchrun/${imageName};image.tag=${imageTag};image.pullPolicy=Always;service.port=${servicePort};livenessProbe.httpGet.path=/health;livenessProbe.httpGet.port=${servicePort};service.type=NodePort",
-        "-n", $imageName
-    )
-    Write-Host "**** Helm Upgrade Command: $helmUpgradeCommand $helmUpgradeArgs ****"
-    Start-Process -FilePath $helmUpgradeCommand -ArgumentList $helmUpgradeArgs -NoNewWindow -Wait
-
+    $helmUpgradeCommand = "helm upgrade $imageName . --set ""image.repository=index.docker.io/dannybatchrun/$imageName,image.tag=$imageTag,image.pullPolicy=Always,service.port=$servicePort,livenessProbe.httpGet.path=/health,livenessProbe.httpGet.port=$servicePort,service.type=NodePort"" -n $imageName"
+    Write-Host "**** Helm Upgrade Command: $helmUpgradeCommand ****"
+    Invoke-Expression $helmUpgradeCommand
     kubectl scale --replicas=1 "deployment/${imageName}" -n "${imageName}"
 }
-upgradeHelmDeployment -imageName "usersubscription" -imageTag "testazure2.0" -servicePort "8090"
-
-
 
 
 
